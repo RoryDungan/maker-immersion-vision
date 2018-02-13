e1bfb3cb0aab4772b62f6dce56c04a9e73458034
Rory Dungan <rorydungan@gmail.com>
2018-02-13 16:45:49 +1000

    Added log message to server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = connect()
 
 server.use(static(__dirname))
 
-server.listen(3000)
+server.listen(3000, undefined, undefined, () => console.log('Server listening on port 3000'))
 
 const livereload = require('livereload')
 const lrserver = livereload.createServer()
