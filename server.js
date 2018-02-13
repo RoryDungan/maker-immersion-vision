bea1b48e313e1f6d00f7f00759b02382ec1fdecf
Rory Dungan <rorydungan@gmail.com>
2018-02-13 13:50:05 +1000

    Set up test server with livereload

---
+++ server.js
@@ -0,0 +1,13 @@
+// Dev server with livereload
+const connect  = require('connect')
+const static = require('serve-static')
+
+const server = connect()
+
+server.use(static(__dirname))
+
+server.listen(3000)
+
+const livereload = require('livereload')
+const lrserver = livereload.createServer()
+lrserver.watch(__dirname)
