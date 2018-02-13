ce590ad0a000d429cbb16fbdc95d0f2d2b9e544f
Rory Dungan <rorydungan@gmail.com>
2018-02-13 14:20:53 +1000

    Made 3d view transparent

--- js/index.js
+++ js/index.js
@@ -53,9 +53,10 @@ function init() {
 
     //
 
-    renderer = new THREE.WebGLRenderer()
+    renderer = new THREE.WebGLRenderer({ alpha: true })
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setClearColor(0x000000, 0)
     container.appendChild(renderer.domElement)
 
     document.addEventListener('mousemove', onDocumentMouseMove, false)
